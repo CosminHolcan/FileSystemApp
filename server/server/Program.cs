54076a371aa875e46da82e34c9ba97c9cf3caf3b
Cosmin Holcan <cosminholcan@gmail.com>
2025-11-11 17:37:11 +0200

    old version

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -1,8 +1,3 @@
-using server.DAL;
-using Microsoft.EntityFrameworkCore;
-using server.BLL;
-using server.Utils;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,49 +7,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
-{
-    builder
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .WithOrigins("http://localhost:3000");
-}));
-
-
-var dbUser = builder.Configuration["dbUser"];
-var dbPassword = builder.Configuration["dbPassword"];
-
-var connectionString = $"Server=tcp:filesystemappdbserver.database.windows.net,1433;Initial Catalog=filesystemappdb;Persist Security Info=False;User ID={dbUser};Password={dbPassword};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-builder.Services.AddDbContext<FileSystemAppDbContext>(options => options.UseSqlServer(connectionString));
-
-builder.Services.AddScoped<UsersDAL>();
-builder.Services.AddScoped<AppFilesDAL>();
-builder.Services.AddScoped<StorageAccountsDAL>();
-builder.Services.AddScoped<FileVersionsDAL>();
-
-builder.Services.AddScoped<UsersBLL>();
-builder.Services.AddScoped<AppFilesBLL>();
-builder.Services.AddScoped<StorageAccountsBLL>();
-builder.Services.AddScoped<FileVersionsBLL>();
-
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    //var db = scope.ServiceProvider.GetRequiredService<FileSystemAppDbContext>();
-    //db.Database.Migrate();
-}
-
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+
 app.UseStaticFiles();
 app.UseHttpsRedirection();
-
-app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
